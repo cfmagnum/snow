1bb3c3f60821a00bca4b8f87c0a0c24e02179427
cfmagnum <mahesh.varak@atos.net>
2017-05-29 15:19:23 +0530

    Add Buildpack App
    Micriservice to call appi for adding buildpack to CF

---
+++ AddBuildpackApp/src/main/java/com/snow/ApiController.java
@@ -0,0 +1,81 @@
+package com.snow;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import org.apache.commons.io.IOUtils;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ApiController {
+	   private String url ="https://api.sys.eu.cfdev.canopy-cloud.com/v2/buildpacks";  
+	   private MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
+	   private String uaaUrl = "http://localhost:8181/Snow-proxy/v2/Authorization";
+	   RestTemplate restTemplate = new RestTemplate();
+	 
+	@RequestMapping("/Snow-proxy/v2/AddBuildpack")   
+	public ResponseEntity<String> Create_Quota_Definition() throws FileNotFoundException, IOException{
+	    String uaatoken =  restTemplate.getForObject(uaaUrl, String.class);
+	    headers.add("Authorization", uaatoken);
+	    headers.add("Content-Type", "application/json");
+	    headers.add("Host", "api.sys.eu.cfdev.canopy-cloud.com");
+	    
+	    String json = IOUtils.toString(new FileInputStream("C:\\workspace\\Add_Buildpack\\AddBuildpackApp\\src\\main\\resources\\BuildpackDetail.json"));
+	    HttpEntity<String> httpEntity = new HttpEntity<>(json, headers);
+	    try {
+			skipSslValidation(url);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return restTemplate.exchange(url, HttpMethod.POST, httpEntity, String.class);
+		
+	}
+	public void skipSslValidation(String ConnectionURL) throws Exception {
+		TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+			public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+				return null;
+			}
+
+			public void checkClientTrusted(X509Certificate[] certs, String authType) {
+			}
+
+			public void checkServerTrusted(X509Certificate[] certs, String authType) {
+			}
+		} };
+
+		// Install the all-trusting trust manager
+		SSLContext sc = SSLContext.getInstance("SSL");
+		sc.init(null, trustAllCerts, new java.security.SecureRandom());
+		HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+		// Create all-trusting host name verifier
+		HostnameVerifier allHostsValid = new HostnameVerifier() {
+			public boolean verify(String hostname, SSLSession session) {
+				return true;
+			}
+		};
+
+		// Install the all-trusting host verifier
+		HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+
+		
+   }
+}
