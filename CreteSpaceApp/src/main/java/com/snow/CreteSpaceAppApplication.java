9cc8c0ef4a5e3a1757bbc8c09862d305d4cbdc65
cfmagnum <mahesh.varak@atos.net>
2017-05-29 15:26:32 +0530

    create space app

---
+++ CreteSpaceApp/src/main/java/com/snow/CreteSpaceAppApplication.java
@@ -0,0 +1,14 @@
+package com.snow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class CreteSpaceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreteSpaceAppApplication.class, args);
+	}
+}
