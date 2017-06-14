7471b37ee9be67948303fee0b67193ce487838a1
Akshay Belose <akshay.belose@atos.net>
2017-06-14 13:06:39 +0530

    success.

--- CreateQuotaDefinition/src/main/java/com/snow/ApiController.java
+++ CreateQuotaDefinition/src/main/java/com/snow/ApiController.java
@@ -4,12 +4,13 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-
 import org.apache.commons.io.IOUtils;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.app.ApplicationInstanceInfo;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,11 +21,17 @@ import org.springframework.web.client.RestTemplate;
 public class ApiController {
 	   private String url ="http://api.sys.eu.cfdev.canopy-cloud.com/v2/quota_definitions";  
 	   private MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
-	   private String uaaUrl = "http://localhost:8181/Snow-proxy/v2/Authorization";
+	  // private String uaaUrl = "http://localhost:8181/Snow-proxy/v2/Authorization";
+	   
+	   private String uaaUrl = "http://uaatokengenerator.apps.eu.cfdev.canopy-cloud.com/v1/get-UAA-token";   
+	   
 	   RestTemplate restTemplate = new RestTemplate();
 	 
+	   @Autowired(required = false) ApplicationInstanceInfo instanceInfo;
+	   
 	@RequestMapping("/Snow-proxy/v2/CreateQuotaDefinition")   
-	public ResponseEntity<String> Create_Quota_Definition() throws FileNotFoundException, IOException{
+	public ResponseEntity<String> Create_Quota_Definition(Model model) throws FileNotFoundException, IOException{
+		model.addAttribute("instanceInfo", instanceInfo);
 	    String uaatoken =  restTemplate.getForObject(uaaUrl, String.class);
 	    headers.add("Authorization", uaatoken);
 	    headers.add("Content-Type", "application/json");
