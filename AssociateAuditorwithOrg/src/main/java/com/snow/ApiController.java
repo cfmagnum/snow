ee4dffeea1acd86fb30ad1529a19b1a92f2913f8
Shital Bansode <shital.bansode@atos.net>
2017-06-14 16:15:03 +0530

    AssociateAuditorwithOrg

--- AssociateAuditorwithOrg/src/main/java/com/snow/ApiController.java
+++ AssociateAuditorwithOrg/src/main/java/com/snow/ApiController.java
@@ -1,20 +1,13 @@
 package com.snow;
 
 
-
 import java.security.cert.X509Certificate;
-
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSession;
 import javax.net.ssl.X509TrustManager;
-
-
-
-
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.app.ApplicationInstanceInfo;
