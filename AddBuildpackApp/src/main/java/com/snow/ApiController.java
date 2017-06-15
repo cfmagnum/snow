22c638e0a89d87173ef46c52650e09aee4b5b02e
cfmagnum <mahesh.varak@atos.net>
2017-06-15 16:50:34 +0530

    mhvn

--- AddBuildpackApp/src/main/java/com/snow/ApiController.java
+++ AddBuildpackApp/src/main/java/com/snow/ApiController.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.security.cert.X509Certificate;
+import java.util.Map;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
@@ -21,30 +22,35 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 public class ApiController {
 	   private String url ="https://api.sys.eu.cfdev.canopy-cloud.com/v2/buildpacks";  
 	   private MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
-	   //private String uaaUrl = "http://localhost:8181/Snow-proxy/v2/Authorization";
+	   
 	   private String uaaUrl = "http://uaatokengenerator.apps.eu.cfdev.canopy-cloud.com/v1/get-UAA-token";
 	   RestTemplate restTemplate = new RestTemplate();
 	   @Autowired(required = false) ApplicationInstanceInfo instanceInfo;
 	   
-	@RequestMapping("/v1/add-build-pack")   
-	public ResponseEntity<String> Create_Quota_Definition(Model model) throws FileNotFoundException, IOException{
+	@RequestMapping(value = "/v1/add-build-pack" ,method = RequestMethod.POST)   
+	public ResponseEntity<String> AddBuildpack(Model model, @RequestBody String json) throws FileNotFoundException, IOException{
 		model.addAttribute("instanceInfo", instanceInfo);
-		ClassLoader classLoader = getClass().getClassLoader();
 	    String uaatoken =  restTemplate.getForObject(uaaUrl, String.class);
+	    ObjectMapper mapper = new ObjectMapper();
+	    Map<String,Object> requestParams = mapper.readValue(json, Map.class);
 	    headers.add("Authorization", uaatoken);
 	    headers.add("Content-Type", "application/json");
 	    headers.add("Host", "api.sys.eu.cfdev.canopy-cloud.com");
 	    
-	    String json = IOUtils.toString(classLoader.getResourceAsStream("BuildpackDetail.json"));
-	    HttpEntity<String> httpEntity = new HttpEntity<>(json, headers);
+	    
+	    HttpEntity<Map<String, Object>> httpEntity = new HttpEntity<>(requestParams, headers);
 	    try {
 			skipSslValidation(url);
 		} catch (Exception e) {
--- AddBuildpackApp/src/main/java/com/snow/Application.java
+++ AddBuildpackApp/src/main/java/com/snow/Application.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-public class AddBuildpackAppApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddBuildpackAppApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
