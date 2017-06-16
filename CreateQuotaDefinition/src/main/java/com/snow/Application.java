0866d8f78a70d8cf6ba0cfa2ad7b6f6f82e3a371
cfmagnum <mahesh.varak@atos.net>
2017-06-16 12:22:47 +0530

    drfvbfd

--- CreateQuotaDefinition/src/main/java/com/snow/Application.java
+++ CreateQuotaDefinition/src/main/java/com/snow/Application.java
@@ -9,10 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-public class CreateQuotaDefinitionApplication {
+public class Application {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		SpringApplication.run(CreateQuotaDefinitionApplication.class, args);
+		SpringApplication.run(Application.class, args);
 		
 	}
 }
