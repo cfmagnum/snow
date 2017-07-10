da09f8fc33e0d884debb7994740186f79c9b8d82
Shital Bansode <shital.bansode@atos.net>
2017-07-10 15:56:39 +0530

    property-file-changes'

--- ListOrgnizations/src/main/java/com/snow/ApiController.java
+++ ListOrgnizations/src/main/java/com/snow/ApiController.java
@@ -4,6 +4,7 @@ package com.snow;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.app.ApplicationInstanceInfo;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -16,18 +17,20 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ApiController {
+	@Autowired
+	Environment env;
 	   private String url ="http://api.sys.eu.cfdev.canopy-cloud.com/v2/organizations";  
 	   private MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
-	   private String uaaUrl = "http://uaatokengenerator.apps.eu.cfdev.canopy-cloud.com/v1/get-UAA-token";
+	  
 	   RestTemplate restTemplate = new RestTemplate();
 	   @Autowired(required = false) ApplicationInstanceInfo instanceInfo;
 	   
 	@RequestMapping("/v1/list-Organizations")   
 	public ResponseEntity<String> getOrgs(Model model){
 		model.addAttribute("instanceInfo", instanceInfo);
-	    String uaatoken =  restTemplate.getForObject(uaaUrl, String.class);
+	    String uaatoken =  restTemplate.getForObject(env.getProperty("uaaUrl"), String.class);
 	    headers.add("Authorization", uaatoken);
-	    headers.add("Content-Type", "application/json");
+	    headers.add("Content-Type", env.getProperty("Content-Type-json"));
 	    HttpEntity<String> requestEntity = new HttpEntity<>("Headers", headers);
 	    return restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class);
 		
