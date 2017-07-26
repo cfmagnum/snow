b8cf5bec797688d95b3a88d05c99a8c1970ca046
cfmagnum <mahesh.varak@atos.net>
2017-07-26 12:58:42 +0530

    general

--- FetchUserInfoApp/src/main/java/com/snow/ApiController.java
+++ FetchUserInfoApp/src/main/java/com/snow/ApiController.java
@@ -1,6 +1,8 @@
 package com.snow;
 
+import java.io.IOException;
 import java.security.cert.X509Certificate;
+import java.util.Map;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
@@ -19,27 +21,39 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 public class ApiController {
 	@Autowired
 	Environment env;
-	private String url = "https://uaa.sys.eu.cfdev.canopy-cloud.com/userinfo";
-	private MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
+	
 	RestTemplate restTemplate = new RestTemplate();
 	@Autowired(required = false)
 	ApplicationInstanceInfo instanceInfo;
 
-	@RequestMapping("/v1/fetch-user-information")
-	public ResponseEntity<String> getuserInfo(Model model) {
+	@RequestMapping(value = "/v1/fetch-user-information" , method = RequestMethod.POST)
+	public ResponseEntity<String> getuserInfo(Model model,@RequestBody String data) throws JsonParseException, JsonMappingException, IOException {
 		model.addAttribute("instanceInfo", instanceInfo);
-		String uaatoken = restTemplate.getForObject(env.getProperty("uaaUrl"),
-				String.class);
-		headers.add("Authorization", uaatoken);
+		String url="";
+	    String clientName="";
+	    String authToken="";
+	    ObjectMapper mapper = new ObjectMapper();
+	    MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
+	    Map<String, Object> requestParams = mapper.readValue(data, Map.class);
+	    authToken=(String) requestParams.get("authToken");
+		clientName =(String) requestParams.get("clientName");
+        url=env.getProperty("url-" +clientName);
+		headers.add("Authorization", authToken);
 
 		restTemplate.setErrorHandler(new DefaultResponseErrorHandler() {
 			protected boolean hasError(HttpStatus statusCode) {
