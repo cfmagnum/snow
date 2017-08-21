229fe695e1d7ad25b336c0c473e3e452247686fc
cfmagnum <mahesh.varak@atos.net>
2017-08-21 14:20:45 +0530

    c vcx

---
+++ CreteSpaceApp/src/main/java/com/snow/ApiController.java
@@ -0,0 +1,394 @@
+package com.snow;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.cert.X509Certificate;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.app.ApplicationInstanceInfo;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+
+@RestController
+public class ApiController {
+	@Autowired
+	Environment env;
+
+	private RestTemplate restTemplate = new RestTemplate();
+	@Autowired(required = false)
+	ApplicationInstanceInfo instanceInfo;
+
+	/**
+	 * @param model
+	 *            -to read vcap parameters to conncet with CF
+	 * @param data
+	 *            -parameters for post request
+	 * @return ResponseEntity<String>
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 *             This method is used to create space
+	 */
+	@RequestMapping(value = "/v1/create-space", method = RequestMethod.POST)
+	public ResponseEntity<String> Create_Space(Model model,
+			@RequestBody String data) throws FileNotFoundException, IOException {
+		model.addAttribute("instanceInfo", instanceInfo);
+package com.snow;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.cert.X509Certificate;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.app.ApplicationInstanceInfo;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+
+@RestController
+public class ApiController {
+	@Autowired
+	Environment env;
+
+	private RestTemplate restTemplate = new RestTemplate();
+	@Autowired(required = false)
+	ApplicationInstanceInfo instanceInfo;
+
+	/**
+	 * @param model
+	 *            -to read vcap parameters to conncet with CF
+	 * @param data
+	 *            -parameters for post request
+	 * @return ResponseEntity<String>
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 *             This method is used to create space
+	 */
+	@RequestMapping(value = "/v1/create-space", method = RequestMethod.POST)
+	public ResponseEntity<String> Create_Space(Model model,
+			@RequestBody String data) throws FileNotFoundException, IOException {
+		model.addAttribute("instanceInfo", instanceInfo);
+
+		Gson gson = new Gson();
+		String url = "";
+		String orgName = "";
+		String orgGuid = "";
+		String spaceName = "";
+		String clientName = "";
+		String authToken = "";
+		String host = env.getProperty("Host-" + clientName);
+		Map<String, String> params = new HashMap<String, String>();
+		ObjectMapper mapper = new ObjectMapper();
+		MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
+
+		Map<String, Object> requestParams = mapper.readValue(data, Map.class);
+		authToken = (String) requestParams.get("authToken");
+		clientName = (String) requestParams.get("clientName");
+		orgName = (String) requestParams.get("organizationName");
+		orgGuid = getOrgGuid(orgName, authToken, host);
+		spaceName = (String) requestParams.get("name");
+		url = env.getProperty("url-" + clientName);
+
+		params.put("organization_guid", orgGuid);
+
+		for (String key : requestParams.keySet()) {
+			if (key != "authToken" && key != "clientName"
+					&& key != "organizationName") {
+				params.put(key, (String) requestParams.get(key));
+			}
+		}
+		String jsonData = gson.toJson(params);
+
+		headers.add("Authorization", authToken);
+		headers.add("Content-Type", env.getProperty("Content-Type-json"));
+		headers.add("Host", host);
+		HttpEntity<String> requestEntity = new HttpEntity<>(jsonData, headers);
+
+		try {
+			skipSslValidation(url);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		restTemplate.setErrorHandler(new DefaultResponseErrorHandler() {
+			protected boolean hasError(HttpStatus statusCode) {
+				return false;
+			}
+		});
+		return restTemplate.exchange(url, HttpMethod.POST, requestEntity,
+				String.class);
+
+	}
+
+	/**
+	 * @param orgName
+	 *            -Name of the Organization
+	 * @param authToken
+	 *            -Authorization token for UAA
+	 * @param host
+	 * @param clientName
+	 * @return String This method returns guid of the organization
+	 */
+	public String getOrgGuid(String orgName, String authToken, String host) {
+		MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
+		String url = "https://api.sys.eu.cfdev.canopy-cloud.com/v2/organizations?q=name:"
+				+ orgName;
+
+		String guid = "";
+		JsonObject resources = new JsonObject();
+		Gson gson = new GsonBuilder().create();
+		JsonObject job = new JsonObject();
+		headers.add("Authorization", authToken);
+		headers.add("Host", host);
+		try {
+			skipSslValidation(url);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		HttpEntity<String> requestEntity = new HttpEntity<>("Headers", headers);
+		String orgInfo = restTemplate.exchange(url, HttpMethod.GET,
+				requestEntity, String.class).getBody();
+		try {
+			job = gson.fromJson(orgInfo, JsonObject.class);
+		} catch (JsonSyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		if (job != null) {
+			if (job.getAsJsonArray("resources") != null) {
+				resources = job.getAsJsonArray("resources").get(0)
+						.getAsJsonObject();
+			}
+
+			JsonObject metadata = resources.get("metadata").getAsJsonObject();
+			guid = metadata.get("guid").getAsString();
+		}
+		return guid;
+	}
+
+	/**
+	 * @param ConnectionURL
+	 * @throws Exception
+	 */
+	public void skipSslValidation(String ConnectionURL) throws Exception {
+		TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+			public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+				return null;
+			}
+
+			public void checkClientTrusted(X509Certificate[] certs,
+					String authType) {
+			}
+
+			public void checkServerTrusted(X509Certificate[] certs,
+					String authType) {
+			}
+		} };
+
+		// Install the all-trusting trust manager
+		SSLContext sc = SSLContext.getInstance("SSL");
+		sc.init(null, trustAllCerts, new java.security.SecureRandom());
+		HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+		// Create all-trusting host name verifier
+		HostnameVerifier allHostsValid = new HostnameVerifier() {
+			public boolean verify(String hostname, SSLSession session) {
+				return true;
+			}
+		};
+
+		// Install the all-trusting host verifier
+		HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+
+	}
+}
+
+		Gson gson = new Gson();
+		String url = "";
+		String orgName = "";
+		String orgGuid = "";
+		String spaceName = "";
+		String clientName = "";
+		String authToken = "";
+		String host = env.getProperty("Host-" + clientName);
+		Map<String, String> params = new HashMap<String, String>();
+		ObjectMapper mapper = new ObjectMapper();
+		MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
+
+		Map<String, Object> requestParams = mapper.readValue(data, Map.class);
+		authToken = (String) requestParams.get("authToken");
+		clientName = (String) requestParams.get("clientName");
+		orgName = (String) requestParams.get("organizationName");
+		orgGuid = getOrgGuid(orgName, authToken, host);
+		spaceName = (String) requestParams.get("name");
+		url = env.getProperty("url-" + clientName);
+
+		params.put("organization_guid", orgGuid);
+
+		for (String key : requestParams.keySet()) {
+			if (key != "authToken" && key != "clientName"
+					&& key != "organizationName") {
+				params.put(key, (String) requestParams.get(key));
+			}
+		}
+		String jsonData = gson.toJson(params);
+
+		headers.add("Authorization", authToken);
+		headers.add("Content-Type", env.getProperty("Content-Type-json"));
+		headers.add("Host", host);
+		HttpEntity<String> requestEntity = new HttpEntity<>(jsonData, headers);
+
+		try {
+			skipSslValidation(url);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		restTemplate.setErrorHandler(new DefaultResponseErrorHandler() {
+			protected boolean hasError(HttpStatus statusCode) {
+				return false;
+			}
+		});
+		return restTemplate.exchange(url, HttpMethod.POST, requestEntity,
+				String.class);
+
+	}
+
+	/**
+	 * @param orgName
+	 *            -Name of the Organization
+	 * @param authToken
+	 *            -Authorization token for UAA
+	 * @param host
+	 * @param clientName
+	 * @return String This method returns guid of the organization
+	 */
+	public String getOrgGuid(String orgName, String authToken, String host) {
+		MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
+		String url = "https://api.sys.eu.cfdev.canopy-cloud.com/v2/organizations?q=name:"
+				+ orgName;
+
+		String guid = "";
+		JsonObject resources = new JsonObject();
+		Gson gson = new GsonBuilder().create();
+		JsonObject job = new JsonObject();
+		headers.add("Authorization", authToken);
+		headers.add("Host", host);
+		try {
+			skipSslValidation(url);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		HttpEntity<String> requestEntity = new HttpEntity<>("Headers", headers);
+		String orgInfo = restTemplate.exchange(url, HttpMethod.GET,
+				requestEntity, String.class).getBody();
+		try {
+			job = gson.fromJson(orgInfo, JsonObject.class);
+		} catch (JsonSyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		if (job != null) {
+			if (job.getAsJsonArray("resources") != null) {
+				resources = job.getAsJsonArray("resources").get(0)
+						.getAsJsonObject();
+			}
+
+			JsonObject metadata = resources.get("metadata").getAsJsonObject();
+			guid = metadata.get("guid").getAsString();
+		}
+		return guid;
+	}
+
+	/**
+	 * @param ConnectionURL
+	 * @throws Exception
+	 */
+	public void skipSslValidation(String ConnectionURL) throws Exception {
+		TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+			public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+				return null;
+			}
+
+			public void checkClientTrusted(X509Certificate[] certs,
+					String authType) {
+			}
+
+			public void checkServerTrusted(X509Certificate[] certs,
+					String authType) {
+			}
+		} };
+
+		// Install the all-trusting trust manager
+		SSLContext sc = SSLContext.getInstance("SSL");
+		sc.init(null, trustAllCerts, new java.security.SecureRandom());
+		HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+		// Create all-trusting host name verifier
+		HostnameVerifier allHostsValid = new HostnameVerifier() {
+			public boolean verify(String hostname, SSLSession session) {
+				return true;
+			}
+		};
+
+		// Install the all-trusting host verifier
+		HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+
+	}
+}
