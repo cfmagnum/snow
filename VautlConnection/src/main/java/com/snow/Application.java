01b4de041fa58851265938abfad0ca0ff6cb6065
cfmagnum <mahesh.varak@atos.net>
2018-01-24 12:28:25 +0530

    acf api new app

--- VautlConnection/src/main/java/com/snow/Application.java
+++ VautlConnection/src/main/java/com/snow/Application.java
@@ -1,5 +1,7 @@
 package com.snow;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +14,22 @@ public class Application {
 
 	public static void main(String[] args) throws VaultException {
 		SpringApplication.run(Application.class, args);
+		int num = 1;
+    	int[] input = {-1,-2,1};
+    	System.out.println(input);
+    	Arrays.sort(input);
+    	int smallest = input[0];
+    	if( smallest<0){
+    		num= 1;
+    	}
+    	if (smallest ==1){
+    	  num = 1;
+    	}
+    	if(smallest >0){
+    	 num = smallest -1;	
+    	}	
+  
+        System.out.println(num);
 	
 	}
 }
---
+++ VautlConnection/src/main/java/com/snow/codility.java
@@ -0,0 +1,23 @@
+package com.snow;
+
+public class codility {
+	
+	
+	    public static void main() {
+	    	int num = 1;
+	    	int[] input = {1,2,4,7};
+	    	int j = 0;
+	    	
+	    	while(input.length==0){
+	    		if(num == input[j]){
+	    			num++;
+	    			j++;
+	    		}
+	    			
+	    		
+	    		
+	    	}
+	        System.out.println(num);
+	    }
+	}
+
